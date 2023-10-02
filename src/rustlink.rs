ac8ef579f3750865a05095e65594780a5948d30b
theodore brockman <tmanbrock@gmail.com>
2023-10-02 00:04:49 -0400

    Unnecessary code since no long using HashSet.

--- src/rustlink.rs
+++ src/rustlink.rs
@@ -1,19 +1,5 @@
-use std::hash::Hash;
-
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Rustlink {
     pub alias: String,
     pub url: Option<String>,
-}
-
-impl PartialEq for Rustlink {
-    fn eq(&self, other: &Self) -> bool {
-        self.alias == other.alias
-    }
-}
-impl Eq for Rustlink {}
-impl Hash for Rustlink {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.alias.hash(state);
-    }
 }
